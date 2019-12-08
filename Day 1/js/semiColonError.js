951d6d435cbce6a0124c6094505d3ac16dae91ec
serrotsabin <sabu27.sabin@gmail.com>
2019-12-08 12:38:01 +0545

    first commit

---
+++ Day 1/js/semiColonError.js
@@ -0,0 +1,13 @@
+//// Part 1
+//[1, 2].forEach(alert)
+
+//// Part 2
+//alert("There will be an error")
+//[1, 2].forEach(alert)
+
+////Part 3
+//alert("All fine now");
+//[1, 2].forEach(alert)
+
+////Part 4 
+//alert("There will be an error")[1, 2].forEach(alert)
